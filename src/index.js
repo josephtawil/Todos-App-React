e805a1aa7690f4f8bf8d825677966c7a9a2eda54
Joseph <jtawil2015@gmail.com>
2020-07-29 18:26:25 -0800

    added welcome function as practice

--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 
+
+function Welcome (props) {
+  return <h1> Welcome, {props.name}</h1>
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <Welcome name = "Joseph"></Welcome> */}
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
